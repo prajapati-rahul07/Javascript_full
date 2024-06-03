19932836fdf5511ce4e155140db07f5885990b01
prajapati-rahul07 <rahulprkumar1997@gmail.com>
2024-06-03 19:58:47 +0000

    datatypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId= 15785
+let accountEmail= "rahul@gmail.com"
+var accountPassword= "12345"
+accountCity= "Bangalore"
+let accountState
+
+/*
+var should not be used to declare a varibale
+*/
+
+
+// accountId= 87324---> constant cannot be reassigned
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // this line means all JS code will be treated as new js version
+
+// number ---> 2 to power 53 
+// bigint 
+// string ---> ""
+// boolean ---> true/flase
+// null ---> standalone value
+// undefined 
+// symbol ---> unique
+
+
+
+let age= 45
+let name= "rahul"
+let loggedin= true
+let weather = null
+let nextIplWinner;
+
+
+
+// object 
+
+console.log(typeof null);
+console.log(typeof undefined);
