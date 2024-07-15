cfcbffbfbce5f8808cfee484d41428f3e1e9a33b
prajapati-rahul07 <rahulprkumar1997@gmail.com>
2024-07-15 13:30:29 +0000

    conversion of data types

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "33whewkj"
+console.log(typeof score)
+
+let newScore = Number(score)
+console.log(typeof newScore)
+
+console.log(newScore)
+
+
+
+// "33" -> 33 that means it converts in Number 
+// 1 ->  true  and 0 -> false 
+// "rahul" ->  true
+
+
+
