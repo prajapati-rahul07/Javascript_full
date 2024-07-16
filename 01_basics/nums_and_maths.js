571d99d057568798a41c20dddf7dda5c5f4e719e
prajapati-rahul07 <rahulprkumar1997@gmail.com>
2024-07-16 12:21:15 +0000

    math and num cimmit

---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,11 @@
+const score = 400.007978990
+console.log(score.toFixed(4));
+const balance = new Number(100)
+console.log(balance);
+
+console.log(Math.round(78987656789.89));
+  
+
+// most common used concept here is Math.random --> which gives a value in between 0 and 1 
+
+console.log(Math.random().toFixed(3));
