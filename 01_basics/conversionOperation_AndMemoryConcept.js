25d47d8a2fa445a25f17b4b85a9a1ff776d582ad
prajapati-rahul07 <rahulprkumar1997@gmail.com>
2024-07-16 09:56:32 +0000

    memory concept

---
+++ 01_basics/conversionOperation_AndMemoryConcept.js
@@ -0,0 +1,26 @@
+let score = "33whewkj"
+console.log(typeof score)
+
+let newScore = Number(score)
+console.log(typeof newScore)
+
+console.log(newScore)
+
+
+
+// "33" -> 33 that means it converts in Number 
+// 1 ->  true  and 0 -> false 
+// "rahul" ->  true
+
+
+
+// There are two kinds of memory prsent in javascript one is Stack Memory and other is Heap Memory
+
+// all the primitives will get memory allocation in stack area and the copy whatever we perform operations on these then   actually a copy of the variable will be provided
+
+// and all the non-primitives will get memory in heap area here the operations will be performed directly on the reference so any change will be affected directly on the variable 
+
+
+
+
+
